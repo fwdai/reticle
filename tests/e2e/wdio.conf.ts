24a5dbf7c99081e8aa7a45dd5baf4b03f43046a6
Alex Chaplinsky <alchaplinsky@gmail.com>
2026-03-10 18:45:03 +0800

    Move e2e to tests folder

--- tests/e2e/wdio.conf.ts
+++ tests/e2e/wdio.conf.ts
@@ -17,7 +17,7 @@ import net from "net";
 import os from "os";
 import path from "path";
 
-const ROOT = path.resolve(__dirname, "..");
+const ROOT = path.resolve(__dirname, "../..");
 
 const APP_BINARY =
   process.env.RETICLE_APP_BINARY ??
